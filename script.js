d9228d748c1b7943ec3ddf7ec828ac96bb0b9ef8
vivi60 <113090754+vivi60@users.noreply.github.com>
2026-01-17 23:37:04 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function checkLogin() {
     if (id === "admin" && pw === "admin0303") {
         localStorage.setItem("loginStatus", "admin"); // 브라우저를 꺼도 남음
         alert("관리자 권한으로 접속합니다.");
-        window.location.href = "homepage/main.html"; // 경로 주의!
+        window.location.href = "main.html"; // 경로 주의!
     } else if (id === "outbreak" && pw === "1234") {
         localStorage.setItem("loginStatus", "success");
         alert("또 그들이 온다.");
-        window.location.href = "homepage/main.html";
+        window.location.href = "main.html";
     } else {
         alert("아이디 또는 비밀번호가 틀렸습니다.");
     }
